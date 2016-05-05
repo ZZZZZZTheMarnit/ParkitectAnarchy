5f4899800b763f253b802c4c7d83450242c0b9c9
Marnit <githubmarnit@gmail.com>
2016-05-05 02:27:13 +0200

    Version 2.2.0
    Settings Panel Update

--- CustomScenery/Main.cs
+++ CustomScenery/Main.cs
@@ -12,6 +12,7 @@ namespace Anarchy
         private GameObject _go;
 		private AnarchyObject Anar;
 		private Dictionary<string, object> anarchy_settings;
+		private Dictionary<string, object> anarchy_strings;
 		private Dictionary<string, string> settings_string = new Dictionary<string, string>();
 		public Dictionary<string, bool> settings_bool = new Dictionary<string, bool>();
 		private Type type;
@@ -24,6 +25,7 @@ namespace Anarchy
 		
 		public void Init() {
 			anarchy_settings = Json.Deserialize(File.ReadAllText(Path + @"/settings.json")) as Dictionary<string, object>;
+			anarchy_strings = Json.Deserialize(File.ReadAllText(Path + @"/dictionary.json")) as Dictionary<string, object>;
 			foreach (KeyValuePair<string, object> S in anarchy_settings) {
 				type = S.Value.GetType();
 				if(type==typeof(bool)) {
@@ -56,12 +58,29 @@ namespace Anarchy
 		
 		public void onDrawSettingsUI()
 		{
+			GUIStyle labelStyle = new GUIStyle (GUI.skin.label); 
+			labelStyle.margin=new RectOffset(15,0,10,0);
+			labelStyle.alignment = TextAnchor.MiddleLeft;
+			GUIStyle toggleStyle = new GUIStyle (GUI.skin.toggle); 
+			toggleStyle.margin=new RectOffset(0,10,19,16);
+			toggleStyle.alignment = TextAnchor.MiddleLeft;
+			GUIStyle textfieldStyle = new GUIStyle (GUI.skin.textField); 
+			textfieldStyle.margin=new RectOffset(0,10,10,0);
+			textfieldStyle.alignment = TextAnchor.MiddleCenter;
+			GUIStyle buttonStyle = new GUIStyle (GUI.skin.button); 
+			buttonStyle.margin=new RectOffset(0,10,10,0);
+			GUILayout.BeginHorizontal();
+			GUILayout.BeginVertical();
+			foreach (KeyValuePair<string, object> S in anarchy_settings) {
+				GUILayout.Label (anarchy_strings[S.Key].ToString(), labelStyle, GUILayout.Height(30));
+			}
+			GUILayout.EndVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginVertical();
 			foreach (KeyValuePair<string, object> S in anarchy_settings) {
 				type = S.Value.GetType();
-				GUILayout.BeginHorizontal();
-				GUILayout.Label (S.Key, GUILayout.Width(175));
 				if(S.Key == "anarchyHotkey") {
-					bool hotkey = GUILayout.Button (settings_string[S.Key], GUILayout.Width(100));
+					bool hotkey = GUILayout.Button (settings_string[S.Key], buttonStyle, GUILayout.Width(130), GUILayout.Height(30));
 					if(hotkey_rebind) {
 						KeyCode key;
 						if (FetchKey (out key)) {
@@ -72,12 +91,13 @@ namespace Anarchy
 						hotkey_rebind = true;
 					}
 				} else if(type==typeof(bool)) {
-					settings_bool[S.Key] = GUILayout.Toggle (settings_bool[S.Key],"", GUILayout.Width(100));
+					settings_bool[S.Key] = GUILayout.Toggle (settings_bool[S.Key],"",toggleStyle, GUILayout.Width(16), GUILayout.Height(16));
 				} else {
-					settings_string[S.Key] = GUILayout.TextField (settings_string[S.Key], GUILayout.Width(100));
+					settings_string[S.Key] = GUILayout.TextField (settings_string[S.Key], textfieldStyle, GUILayout.Width(130), GUILayout.Height(30));
 				}
-				GUILayout.EndHorizontal();
 			}
+			GUILayout.EndVertical();
+			GUILayout.EndHorizontal();
 		}
 		
 		private bool FetchKey(out KeyCode outKey)
@@ -141,7 +161,7 @@ namespace Anarchy
 			}
 		}
 		
-        public string Name { get { return "Construction Anarchy"; } }
+        public string Name { get { return "Construction Anarchy [v2.2.0]"; } }
         public string Description { get { return "Lifts building restrictions for assets."; } }
         private string path;
 		public string Path {
